0ea8388410721410a3b4c9cc024883f045d4e101
PatelPriyanshu-24 <159017504+PatelPriyanshu-24@users.noreply.github.com>
2024-04-08 17:00:28 +0530

    update

--- backend/index.ts
+++ backend/index.ts
@@ -15,25 +15,14 @@ dbConnect()
 const port = 5000;
 const app = express();
 app.use(express.json())
-// app.use(
-//   cors({
-//     credentials: true,
-//     origin: ["http://localhost:4200"],
-//     origin: ["https://gofood-sepia.vercel.app"],
+app.use(
+  cors({
+    credentials: true,
+    // origin: ["http://localhost:4200"],
+    origin: ["https://gofood-sepia.vercel.app"],
     
-//   })
-// );
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://gofood-sepia.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
+  })
+);
 
 
 app.use("/api/food",FoodRouter);
